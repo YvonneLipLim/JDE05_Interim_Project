7a4006b7ddcc6b58d7485e986643f1d43f95254f
Erica Lee <nihsha41@gmail.com>
2024-11-26 05:45:35 +0800

    Update data_dictionary_script.sql
    Column ALIAS, Add Primary Key Column

--- Reference/data_dictionary_script.sql
+++ Reference/data_dictionary_script.sql
@@ -1,4 +1,6 @@
-select table_name,column_name,pgd.description,udt_name,character_maximum_length,data_type
+select table_name AS "Table Name",column_name AS "Column Name",
+pgd.description AS "Description",udt_name AS "UDT Name",character_maximum_length AS "Max.Length",data_type AS "Data Type",
+is_identity AS "Primary Key"
 from pg_catalog.pg_statio_all_tables as st
 inner join pg_catalog.pg_description pgd on (
     pgd.objoid = st.relid
